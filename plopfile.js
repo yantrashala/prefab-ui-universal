b572dbb9f70fcf8270072b98b5bb58d9febaca9d
Vinci Rufus <vinci.rufus@gmail.com>
2019-03-15 11:13:12 +0530

    adding CLI generator and templates

---
+++ plopfile.js
@@ -0,0 +1,48 @@
+module.exports = function(plop) {
+  // create your generators here
+  plop.setGenerator('prefab', {
+    description: 'Generates the Prefab UI',
+    prompts: [
+      {
+        type: 'input',
+        name: 'project-name',
+        message: 'Name of your project'
+      }
+    ], // array of inquirer prompts
+    actions: [
+      {
+        type: 'add',
+        path: 'packages/app/package.json',
+        templateFile: 'plop-templates/app/package.json'
+      },
+      {
+        type: 'add',
+        path: 'packages/app/pages/index.js',
+        templateFile: 'plop-templates/app/pages/index.js'
+      },
+      {
+        type: 'add',
+        path: 'packages/app/pages/about.js',
+        templateFile: 'plop-templates/app/pages/about.js'
+      },
+      {
+        type: 'modify',
+        path: 'packages/app/package.json',
+        pattern: /##__PROJECT_NAME_DASH_CASE__##/gi,
+        template: '{{dashCase project-name}}'
+      },
+      {
+        type: 'modify',
+        path: 'packages/app/pages/about.js',
+        pattern: /##__PROJECT_NAME_DASH_CASE__##/gi,
+        template: '{{titleCase project-name}}'
+      },
+      {
+        type: 'modify',
+        path: 'packages/app/pages/index.js',
+        pattern: /##__PROJECT_NAME_DASH_CASE__##/gi,
+        template: '{{titleCase project-name}}'
+      }
+    ] // array of actions
+  });
+};
