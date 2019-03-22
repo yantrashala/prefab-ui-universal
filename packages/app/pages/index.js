a102275ff7404b355af33c9db48d41ee248698fb
vyom00 <awslabs00@gmail.com>
2019-03-22 11:28:28 +0530

    first-change (#2)
    * first-commit
    
    * jenkinsfile-update
    
    * changing the app name
    
    * Update index.js

--- packages/app/pages/index.js
+++ packages/app/pages/index.js
@@ -1,11 +1,12 @@
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import BillBoard from "../components/Billboard";
-import { Reaction } from "mobx";
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import BillBoard from '../components/Billboard';
+import { Reaction } from 'mobx';
 
 const Index = () => (
   <React.Fragment>
     <Header />
+    <h1> Welcome to Fab - Universal</h1>
     <Banner />
     <BillBoard />
   </React.Fragment>
